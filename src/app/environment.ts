63962d6d52019029f9afe4fa079a78a80df764eb
SimpleHacker409 <manjunath.siddesh@studenti.unimi.it>
2023-02-07 13:47:10 +0100

    before update angular
    Signed-off-by: SimpleHacker409 <manjunath.siddesh@studenti.unimi.it>

---
+++ src/app/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAOGVOZZ5vWEzLHJDmT9LhDeC9O6SQzRyc",
+    authDomain: "evolvodev-4bfe3.firebaseapp.com",
+    projectId: "evolvodev-4bfe3",
+    storageBucket: "evolvodev-4bfe3.appspot.com",
+    messagingSenderId: "821921832634",
+    appId: "1:821921832634:web:1ffcd895bd6fdb32772c83"
+  },
+  production: false,
+};
